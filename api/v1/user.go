7b391d1a7ccf311f9ef5b59ea4dc6b05a9b5480f
palaemonboy <sgzycc@gmail.com>
2022-07-17 00:33:39 +0800

    add all user interface

--- api/v1/user.go
+++ api/v1/user.go
@@ -6,7 +6,13 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
+
+type QueryUserListReq struct {
+	PageSize int `form:"page_size"`
+	PageNum  int `form:"page_num"`
+}
 
 var code int
 
@@ -26,6 +32,7 @@ func AddNewUser(c *gin.Context) {
 	}
 	c.JSON(http.StatusOK, gin.H{
 		"status":  code,
+		"data":    data,
 		"message": errormsg.GetErrMsg(code),
 	})
 }
@@ -34,15 +41,55 @@ func AddNewUser(c *gin.Context) {
 
 // QueryUserList 查询用户列表 | query user list
 func QueryUserList(c *gin.Context) {
-
+	var req QueryUserListReq
+	if err := c.ShouldBindQuery(&req); err != nil {
+		fmt.Println("Get QueryUserListReq error:", err)
+		return
+	}
+	fmt.Println(req.PageSize, req.PageNum)
+	if req.PageSize == 0 {
+		req.PageSize = -1
+	}
+	if req.PageNum == 0 {
+		req.PageNum = -1
+	}
+	data := model.GetUserList(req.PageSize, req.PageNum)
+	code = errormsg.SUCCESS
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"data":    data,
+		"message": errormsg.GetErrMsg(code),
+	})
 }
 
 // EditUser 编辑用户 | edit user
 func EditUser(c *gin.Context) {
-
+	var data model.User
+	id, _ := strconv.Atoi(c.Param("id"))
+	if err := c.ShouldBindJSON(&data); err != nil {
+		fmt.Println("Get JSON error:", err)
+		return
+	}
+	code := model.CheckUserStatus(data.Username)
+	if code == errormsg.SUCCESS {
+		model.EditUser(id, &data)
+	}
+	if code == errormsg.ErrorUserNameUsed {
+		c.Abort()
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"message": errormsg.GetErrMsg(code),
+	})
 }
 
 // DeleteUser 删除用户 | delete user
 func DeleteUser(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Param("id"))
+	code := model.DeleteUser(id)
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"message": errormsg.GetErrMsg(code),
+	})
 
 }
--- model/user.go
+++ model/user.go
@@ -2,8 +2,10 @@ package model
 
 import (
 	"PalaemonBlog/utils/errormsg"
-	"github.com/gin-gonic/gin"
+	"encoding/base64"
+	"golang.org/x/crypto/scrypt"
 	"gorm.io/gorm"
+	"log"
 )
 
 type User struct {
@@ -13,7 +15,7 @@ type User struct {
 	Role     int    `gorm:"type:int" json:"role" `
 }
 
-// 查询用户状态 | query user status
+// CheckUserStatus 查询用户状态 | query user status
 func CheckUserStatus(name string) (code int) {
 	var user User
 	Db.Select("id").Where("username = ?", name).First(&user)
@@ -26,6 +28,7 @@ func CheckUserStatus(name string) (code int) {
 
 // CreateNewUser 添加新用户 | add new user
 func CreateNewUser(data *User) int {
+	data.Password = ScryptPw(data.Password)
 	err := Db.Create(&data)
 	if err != nil {
 		return errormsg.ERROR
@@ -35,17 +38,46 @@ func CreateNewUser(data *User) int {
 
 // 查询单个用户 | query user
 
-// 查询用户列表 | query user list
-func QueryUserList(c *gin.Context) {
-
+// GetUserList 查询用户列表 | query user list
+func GetUserList(PageSize int, PageNum int) []User {
+	var users []User
+	err = Db.Limit(PageSize).Offset((PageNum - 1) * PageSize).Find(&users).Error
+	if err != gorm.ErrRecordNotFound && err != nil {
+		return nil
+	}
+	return users
 }
 
-// 编辑用户 | edit user
-func EditUser(c *gin.Context) {
-
+// EditUser 编辑用户 | edit user
+func EditUser(ID int, data *User) (code int) {
+	var maps = make(map[string]interface{})
+	maps["username"] = data.Username
+	maps["role"] = data.Role
+	err = Db.Model(&User{}).Where("id = ?", ID).Updates(maps).Error
+	if err != nil {
+		return errormsg.ERROR
+	}
+	return errormsg.SUCCESS
 }
 
-// 删除用户 | delete user
-func DeleteUser(c *gin.Context) {
+// DeleteUser 删除用户 | delete user
+func DeleteUser(ID int) (code int) {
+	var user User
+	err := Db.Where("id = ?", ID).Delete(&user).Error
+	if err != nil {
+		return errormsg.ERROR
+	}
+	return errormsg.SUCCESS
+}
 
+// ScryptPw 密码加密 | Password encryption
+func ScryptPw(password string) string {
+	salt := []byte{0xc8, 0x28, 0xf2, 0x58, 0xa7, 0x6a, 0xad, 0x7b}
+	keyLen := 10
+	pwHash, err := scrypt.Key([]byte(password), salt, 1024, 8, 1, keyLen)
+	if err != nil {
+		log.Fatal("Password encryption error", err)
+	}
+	finalPw := base64.StdEncoding.EncodeToString(pwHash)
+	return finalPw
 }
